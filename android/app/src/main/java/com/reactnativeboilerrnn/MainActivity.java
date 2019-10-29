187a65a5f4a79c2962c08116f9f96dd368c88bd9
Dmitry Kazinets <qdsoftinfo@gmail.com>
2019-10-29 11:26:22 +0300

    update dependencies to 0.61

---
+++ android/app/src/main/java/com/reactnativeboilerrnn/MainActivity.java
@@ -0,0 +1,6 @@
+package com.reactnativeboilerrnn;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+}
--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-// import { AppRegistry } from 'react-native';s
 import './src';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
